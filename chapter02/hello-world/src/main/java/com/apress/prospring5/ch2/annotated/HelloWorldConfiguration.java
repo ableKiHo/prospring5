d898036be21b62bbf905194965e0c2d4088e2ccf
ukh <able.kiho.um@gmail.com>
2019-12-18 23:19:11 +0900

    2.4.3 애너테이션을 사용한 스프링 구성

---
+++ chapter02/hello-world/src/main/java/com/apress/prospring5/ch2/annotated/HelloWorldConfiguration.java
@@ -0,0 +1,26 @@
+package com.apress.prospring5.ch2.annotated;
+
+import com.apress.prospring5.ch2.decoupled.HelloWorldMessageProvider;
+import com.apress.prospring5.ch2.decoupled.MessageProvider;
+import com.apress.prospring5.ch2.decoupled.MessageRenderer;
+import com.apress.prospring5.ch2.decoupled.StandardOutMessageRenderer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfiguration {
+
+    // <bean id="provider" class="..." />
+    @Bean
+    public MessageProvider provider() {
+        return new HelloWorldMessageProvider();
+    }
+
+    // <bean id="renderer" class="..." />
+    @Bean
+    public MessageRenderer renderer() {
+        MessageRenderer renderer = new StandardOutMessageRenderer();
+        renderer.setMessageProvider(provider());
+        return renderer;
+    }
+}
