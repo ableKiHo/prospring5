87194f76df6478edcb74c41cbcc6ab4fed16d396
ukh <able.kiho.um@gmail.com>
2019-12-19 00:34:54 +0900

    3.2.1 의존성 풀(dependency-pull) 3.2.2 문맥에 따른 의존성 룩업(CDL)

---
+++ chapter03/cdl/src/main/java/com/apress/prospring5/ch3/Container.java
@@ -0,0 +1,6 @@
+package com.apress.prospring5.ch3;
+
+public interface Container {
+    Object getDependency(String key);
+
+}
---
+++ chapter03/cdl/src/main/java/com/apress/prospring5/ch3/ManagedComponent.java
@@ -0,0 +1,7 @@
+package com.apress.prospring5.ch3;
+
+import java.awt.*;
+
+public interface ManagedComponent {
+    void performLookup(Container container);
+}
